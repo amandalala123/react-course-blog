8f9dd892d9dfa522d884957250804c0ac29b29dc
Amanda <amandagoodrick@gmail.com>
2019-11-12 17:04:09 -0800

    Handle 404 case

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -13,6 +13,7 @@ class NewPost extends Component {
     }
 
     componentDidMount () {
+        // If unauth => this.props.history.replace('/posts');
         console.log(this.props);
     }
 
@@ -25,7 +26,7 @@ class NewPost extends Component {
         axios.post('/posts', data)
             .then(response => {
                 console.log(response);
-                this.props.history.push('/posts');
+                this.props.history.replace('/posts');
                 // this.setState({submitted: true});
             });
     }
